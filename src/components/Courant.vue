67e054a5db24e18b3e84f489cfeda6de3a94566e
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-07-04 16:44:39 +0200

    04/07/2018 MAJ GENERALE

--- src/components/Courant.vue
+++ src/components/Courant.vue
@@ -40,19 +40,17 @@ import Icon1 from "../assets/credit-card.png"
 
 export default {
   name: "Courant",
-  data () {
+  data() {
     return {
-      comptes: [
-        {
-          name: 'One',
-          class: "text-success",
-          imgSource: Icon1,
-          content: "Compte courant",
-          montant: "3,247",
-          type: "EUR",
-          spec: "Dernière consultation: Vendredi 12 Septembre à 14h02"
-        }
-      ],
+      comptes: [{
+        name: 'One',
+        class: "text-success",
+        imgSource: Icon1,
+        content: "Compte courant",
+        montant: "3,247",
+        type: "EUR",
+        spec: "Dernière consultation: Vendredi 12 Septembre à 14h02"
+      }],
       titres: [
         ["Date", "disappear"],
         ["Libellé"],
@@ -61,196 +59,193 @@ export default {
         ["Débit"],
         ["Crédit"]
       ],
-      operations: [
-        {
-          date: "10/09/2018",
-          libelle: "MONOP', PARIS 15",
-          ref: 5699856,
-          dateOperation: "10/09/2018, 08h32",
-          debit: 13.11,
-          credit: ""
-        },{
-          date: "10/09/2018",
-          libelle: "LA POSTE BEAUGRENELLE, PARIS 15",
-          ref: 5699822,
-          dateOperation: "12/09/2018, 08h17",
-          debit: 9.25,
-          credit: ""
-        },{
-          date: "10/09/2018",
-          libelle: "UBER",
-          ref: 5699842,
-          dateOperation: "12/09/2018, 08h05",
-          debit: 29.78,
-          credit: ""
-        },{
-          date: "09/09/2018",
-          libelle: "FNAC, PARIS 15",
-          ref: 5699827,
-          dateOperation: "09/09/2018, 18h30",
-          debit: 19.99,
-          credit: ""
-        },{
-          date: "08/09/2018",
-          libelle: "CAFE DU COMMERCE, PARIS 15",
-          ref: 5692727,
-          dateOperation: "08/09/2018, 12h53",
-          debit: 12.25,
-          credit: ""
-        },{
-          date: "06/09/2018",
-          libelle: "AMAZON REF*98988 REMBOURSEMENT",
-          ref: 5699875,
-          dateOperation: "03/09/2018, 00h00",
-          debit: "",
-          credit: 95.50
-        },{
-          date: "05/09/2018",
-          libelle: "RED BY SFR",
-          ref: 5699885,
-          dateOperation: "05/09/2018, 07h01",
-          debit: 3.25,
-          credit: ""
-        },{
-          date: "05/09/2018",
-          libelle: "EDF",
-          ref: 5699888,
-          dateOperation: "05/09/2018, 06h32",
-          debit: 11.22,
-          credit: ""
-        },{
-          date: "02/09/2018",
-          libelle: "RET DAB PARIS ZOLA-COMMERCE",
-          ref: 5699868,
-          dateOperation: "02/09/2018, 15h32",
-          debit: "50.00",
-          credit: ""
-        },{
-          date: "02/09/2018",
-          libelle: "KEEP COOL",
-          ref: 5699854,
-          dateOperation: "02/09/2018, 12h10",
-          debit: 36.25,
-          credit: ""
-        }
-      ]
+      operations: [{
+        date: "10/09/2018",
+        libelle: "MONOP', PARIS 15",
+        ref: 5699856,
+        dateOperation: "10/09/2018, 08h32",
+        debit: 13.11,
+        credit: ""
+      }, {
+        date: "10/09/2018",
+        libelle: "LA POSTE BEAUGRENELLE, PARIS 15",
+        ref: 5699822,
+        dateOperation: "12/09/2018, 08h17",
+        debit: 9.25,
+        credit: ""
+      }, {
+        date: "10/09/2018",
+        libelle: "UBER",
+        ref: 5699842,
+        dateOperation: "12/09/2018, 08h05",
+        debit: 29.78,
+        credit: ""
+      }, {
+        date: "09/09/2018",
+        libelle: "FNAC, PARIS 15",
+        ref: 5699827,
+        dateOperation: "09/09/2018, 18h30",
+        debit: 19.99,
+        credit: ""
+      }, {
+        date: "08/09/2018",
+        libelle: "CAFE DU COMMERCE, PARIS 15",
+        ref: 5692727,
+        dateOperation: "08/09/2018, 12h53",
+        debit: 12.25,
+        credit: ""
+      }, {
+        date: "06/09/2018",
+        libelle: "AMAZON REF*98988 REMBOURSEMENT",
+        ref: 5699875,
+        dateOperation: "03/09/2018, 00h00",
+        debit: "",
+        credit: 95.50
+      }, {
+        date: "05/09/2018",
+        libelle: "RED BY SFR",
+        ref: 5699885,
+        dateOperation: "05/09/2018, 07h01",
+        debit: 3.25,
+        credit: ""
+      }, {
+        date: "05/09/2018",
+        libelle: "EDF",
+        ref: 5699888,
+        dateOperation: "05/09/2018, 06h32",
+        debit: 11.22,
+        credit: ""
+      }, {
+        date: "02/09/2018",
+        libelle: "RET DAB PARIS ZOLA-COMMERCE",
+        ref: 5699868,
+        dateOperation: "02/09/2018, 15h32",
+        debit: "50.00",
+        credit: ""
+      }, {
+        date: "02/09/2018",
+        libelle: "KEEP COOL",
+        ref: 5699854,
+        dateOperation: "02/09/2018, 12h10",
+        debit: 36.25,
+        credit: ""
+      }]
     }
   }
 }
 </script>
 
 <style lang="scss">
-
-  #Courant {
-    padding:25px 50px;
-    max-width:1200px;
-    width:calc(100% - 300px);
+#Courant {
+    padding: 25px 50px;
+    max-width: 1200px;
+    width: calc(100% - 300px);
     .first {
-      padding-left:0;
-      @media screen and (max-width: 1020px) {
-        padding:0;
-      }
+        padding-left: 0;
+        @media screen and (max-width: 1020px) {
+            padding: 0;
+        }
     }
     .special {
-      padding-right:0px;
-      margin-right:0px;
-      @media screen and (max-width: 1020px) {
-        padding:0;
-      }
+        padding-right: 0;
+        margin-right: 0;
+        @media screen and (max-width: 1020px) {
+            padding: 0;
+        }
     }
     .theme-blue {
-      margin:25px 0;
+        margin: 25px 0;
     }
     .box__icon {
-      height:60px;
-      img {
-        height:100%;
-      }
+        height: 60px;
+        img {
+            height: 100%;
+        }
     }
     .row {
-      width:100%;
-      margin-left:0;
-      margin-right:0;
-      @media screen and (max-width: 780px) {
-        padding:0;
-        margin:0;
-      }
-      .box__module {
-        .image {
-          width:75px;
-          border-radius:50px;
-          overflow:hidden;
-          img {
-            width:100%;
-          }
-        }
-      }
-      .btn {
-        font-size:1.2em;
-        padding:0 6px;
-        cursor:pointer;
-      }
-      .btn:hover {
-        color:#2B55A0;
-      }
-      .options {
-        display: flex;
-        flex-direction: row;
+        width: 100%;
+        margin-left: 0;
+        margin-right: 0;
         @media screen and (max-width: 780px) {
-          flex-direction: column;
+            padding: 0;
+            margin: 0;
         }
-      }
+        .box__module {
+            .image {
+                width: 75px;
+                border-radius: 50px;
+                overflow: hidden;
+                img {
+                    width: 100%;
+                }
+            }
+        }
+        .btn {
+            font-size: 1.2em;
+            padding: 0 6px;
+            cursor: pointer;
+        }
+        .btn:hover {
+            color: #2B55A0;
+        }
+        .options {
+            display: flex;
+            flex-direction: row;
+            @media screen and (max-width: 780px) {
+                flex-direction: column;
+            }
+        }
     }
     #charts {
-      div:first-child {
-        margin-right:10px;
-      }
+        div:first-child {
+            margin-right: 10px;
+        }
     }
     @media screen and (max-width: 780px) {
-      margin-top:110px;
-      padding:10px;
-      width:100%;
+        margin-top: 110px;
+        padding: 10px;
+        width: 100%;
     }
     table {
-      width:100%;
+        width: 100%;
     }
     td {
-      @media screen and (max-width: 780px) {
-        margin:0 20px;
-      }
+        @media screen and (max-width: 780px) {
+            margin: 0 20px;
+        }
     }
     tr {
-      font-size:.9em;
-      line-height:2.5;
-      border-bottom:1px solid rgba(245, 245, 245, .5);
-        @media screen and (max-width: 780px) {
+        font-size: 0.9em;
+        line-height: 2.5;
+        border-bottom: 1px solid rgba(245, 245, 245, .5);
+        @media screen and (max-width: 780px) {}
+        a {
+            font-weight: 400;
+            color: #2261d0;
+            transition: color 0.2s;
         }
-      a {
-        font-weight:400;
-        color:#2261d0;
-        transition:color .2s;
-      }
-      a:hover {
-        color:#F8880F;
-        text-decoration:none;
-        transition:color .2s;
-      }
+        a:hover {
+            color: #F8880F;
+            text-decoration: none;
+            transition: color 0.2s;
+        }
     }
-    .date, .ref {
-      color:lightgrey;
-      font-weight:100;
+    .date,
+    .ref {
+        color: lightgrey;
+        font-weight: 100;
     }
-    .debit, .credit {
-      font-weight:400;
+    .credit,
+    .debit {
+        font-weight: 400;
     }
     .debit {
-      color:#2261d0;
+        color: #2261d0;
     }
     .disappear {
-      @media screen and (max-width: 780px) {
-          display:none;
-      }
+        @media screen and (max-width: 780px) {
+            display: none;
+        }
     }
-  }
-
+}
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,124 +1,128 @@
 <template>
-  <div id="Navigation">
-    <div class="logo___responsive">
-      <img src="../assets/logo.png" alt="Banque Populaire Occitane" id="logo__responsive__general" v-show="burger">
-    </div>
-    <div class="box box_logo">
-      <img src="../assets/logo.png" alt="Banque Populaire Occitane" id="logo">
-    </div>
-    <nav class="menu">
-      <ul>
-          <li v-for="option in menu" v-bind:class="option.class">
-            <router-link :to="option.link">
-            {{ option.content }}
-            <i class="fleche fas fa-arrow-right"></i>
-            </router-link>
-          </li>
-      </ul>
-    </nav>
-    <nav class="box menu_link">
-      <ul>
-        <li v-for="link in menu_link">
-          {{ link.content }}
-        </li>
-      </ul>
-    </nav>
-    <div id="log__out" @click="show()">
-      <p>Se déconnecter</p>
-      <img src="../assets/logout.png" alt="logout">
-    </div>
-    <img src="../assets/menu.png" class="cross__menu" @click="toggleMenu()" v-show="burger" />
-    <img src="../assets/cancel.png" class="cross__menu" id="cross" @click="closeMenu()" />
-    <modal name="logout">
-      <div class="question__modal">
-        <p>Voulez-vous vous déconnecter?</p>
-        <div class="btn blue__button bouton" @click="hide()">
-          Oui
-        </div>
-        <div class="btn blue__button bouton" @click="hide()">
-          Non
-        </div>
-      </div>
-    </modal>
-    <modal name="mail">
-      <div class="question__modal">
-
-      </div>
-    </modal>
+<div id="Navigation">
+  <div class="logo___responsive" v-show="burger">
+    <img src="../assets/logo.png" alt="Banque Populaire Occitane" id="logo__responsive__general" v-show="burger">
   </div>
+  <div class="box box_logo">
+    <img src="../assets/logo.png" alt="Banque Populaire Occitane" id="logo">
+  </div>
+  <nav class="menu">
+    <ul>
+      <li v-for="option in menu" v-bind:class="option.class" @click="closeMenu()">
+        <router-link :to="option.link">
+          {{ option.content }}
+          <i class="fleche fas fa-arrow-right"></i>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+  <nav class="box menu_link">
+    <ul>
+      <li v-for="link in menu_link">
+        {{ link.content }}
+      </li>
+    </ul>
+  </nav>
+  <div id="log__out" @click="show()">
+    <p>Se déconnecter</p>
+    <img src="../assets/logout.png" alt="logout">
+  </div>
+  <img src="../assets/menu.png" class="cross__menu" @click="toggleMenu()" v-show="burger" />
+  <img src="../assets/cancel.png" class="cross__menu" id="cross" @click="closeMenu()" />
+  <modal name="logout">
+    <div class="question__modal">
+      <p>Voulez-vous vous déconnecter?</p>
+      <div class="btn blue__button bouton" @click="hide()">
+        Oui
+      </div>
+      <div class="btn blue__button bouton" @click="hide()">
+        Non
+      </div>
+    </div>
+  </modal>
+  <modal name="mail">
+    <div class="question__modal">
+
+    </div>
+  </modal>
+  <div class="social">
+    <a href="https://linkedin.com/in/guillaumeduhan" target="_blank">
+        <img src="../assets/linkedin-logo.png" alt="linkedin">
+      </a>
+    <a href="https://github.com/guillaumeduhan/BANQUE-POP-APP-WEB" target="_blank">
+        <img src="../assets/github.png" alt="git">
+      </a>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'Navigation',
-  data () {
+  data() {
     return {
       burger: true,
-      menu: [
-        {
-          content: "Mon espace",
-          link: "/",
-          class: ""
-        },{
-          content: "Mes comptes",
-          link: "/compte-courant",
-          class: ""
-        },{
-          content: "Mes opérations",
-          link: "/",
-          class: ""
-        },{
-          content: "Gérer au quotidien",
-          link: "/",
-          class: ""
-        },{
-          content: "Épargner",
-          link: "/",
-          class: ""
-        },{
-          content: "Emprunter/Assurer",
-          link: "/",
-          class: ""
-        },
-      ],
-      menu_link: [
-        {
-          content:"Votre banque",
-          link:"/"
-        },{
-          content:"Service informatique",
-          link:"/"
-        },{
-          content:"Information pratique",
-          link:"/"
-        },{
-          content:"Tarifs et réglementation",
-          link:"/"
-        },{
-          content:"Banque en ligne",
-          link:"/"
-        },{
-          content:"Se déconnecter",
-          link:"/"
-        }
-      ]
+      menu: [{
+        content: "Mon espace",
+        link: "/",
+        class: ""
+      }, {
+        content: "Mes comptes",
+        link: "/compte-courant",
+        class: ""
+      }, {
+        content: "Mes opérations",
+        link: "/",
+        class: ""
+      }, {
+        content: "Gérer au quotidien",
+        link: "/",
+        class: ""
+      }, {
+        content: "Épargner",
+        link: "/",
+        class: ""
+      }, {
+        content: "Emprunter/Assurer",
+        link: "/",
+        class: ""
+      }, ],
+      menu_link: [{
+        content: "Votre banque",
+        link: "/"
+      }, {
+        content: "Service informatique",
+        link: "/"
+      }, {
+        content: "Information pratique",
+        link: "/"
+      }, {
+        content: "Tarifs et réglementation",
+        link: "/"
+      }, {
+        content: "Banque en ligne",
+        link: "/"
+      }, {
+        content: "Se déconnecter",
+        link: "/"
+      }]
     }
   },
   methods: {
-    toggleMenu: function(){
+    toggleMenu: function() {
       let navigation = window.document.getElementById('Navigation');
       navigation.style.left = "0px";
       this.burger = !this.burger;
     },
-    closeMenu: function(){
+    closeMenu: function() {
       let navigation = window.document.getElementById('Navigation');
       navigation.style.left = "-300px";
       this.burger = !this.burger;
     },
-    show:function(){
+    show: function() {
       this.$modal.show('logout');
     },
-    hide:function(){
+    hide: function() {
       this.$modal.hide('logout');
     }
   }
@@ -126,172 +130,181 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 #Navigation {
-  font-family: "Open Sans", sans-serif;
-  display:flex;
-  flex-flow: column;
-  width:300px;
-  height: 100vh;
-  transition:1s;
-  background-color:white;
-  height:-webkit-fill-available;
-  -webkit-box-shadow: 0px 0px 35px 5px rgba(0,0,0,0.045);
-  -moz-box-shadow: 0px 0px 35px 5px rgba(0,0,0,0.045);
-  box-shadow: 0px 0px 35px 5px rgba(0,0,0,0.045);
-  .logo___responsive {
-    display:none;
-    position:absolute;
-    left:300px;
-    top:0px;
-    width:100%;
-    padding: 20px 20px 20px 75px;
-    #logo__responsive__general {
-      @media screen and (max-width: 780px) {
-      }
+    font-family: "Open Sans", sans-serif;
+    display: flex;
+    flex-flow: column;
+    width: 300px;
+    height: 100vh;
+    transition: 1s;
+    background-color: white;
+    height: -webkit-fill-available;
+    -webkit-box-shadow: 0 0 35px 5px rgba(0,0,0,0.045);
+    -moz-box-shadow: 0 0 35px 5px rgba(0,0,0,0.045);
+    box-shadow: 0 0 35px 5px rgba(0,0,0,0.045);
+    .logo___responsive {
+        display: none;
+        position: absolute;
+        left: 300px;
+        top: 0;
+        width: 100%;
+        padding: 20px 20px 20px 75px;
+        #logo__responsive__general {
+            @media screen and (max-width: 780px) {}
+        }
+        @media screen and (max-width: 780px) {
+            display: block;
+        }
     }
-    @media screen and (max-width: 780px) {
-      display:block;
+    img {
+        max-width: 100%;
     }
-  }
-  img {
-    max-width:100%;
-  }
-  .box {
-    padding:25px 35px;
-    @media screen and (max-width: 780px) {
-      padding:0px 35px;
+    .box {
+        padding: 25px 35px;
+        @media screen and (max-width: 780px) {
+            padding: 0 35px;
+        }
     }
-  }
-  .box_logo {
-    padding:50px 35px;
-    @media screen and (max-width: 780px) {
-      padding:20px 70px 20px 25px;
+    .box_logo {
+        padding: 50px 35px;
+        @media screen and (max-width: 780px) {
+            padding: 20px 70px 20px 25px;
+        }
     }
-  }
-  .menu {
-    ul {
-      list-style-type:none;
-      padding:0;
-      li a{
-        display: flex;
-        text-decoration: none;
+    .menu {
+        ul {
+            list-style-type: none;
+            padding: 0;
+            li a {
+                display: flex;
+                text-decoration: none;
+                justify-content: left;
+                align-items: center;
+                font-size: 16px;
+                font-weight: 100;
+                color: white;
+                padding: 12px 0;
+                margin: 8px 0;
+                background-color: #2B55A0;
+                width: 235px;
+                padding-left: 35px;
+                border-top-right-radius: 25px;
+                border-bottom-right-radius: 25px;
+                transition: 0.25s;
+                .fleche {
+                    opacity: 0;
+                    left: 195px;
+                }
+            }
+            li a:hover {
+                background-color: #F8880F;
+                transition: 0.25s;
+                width: 250px;
+                cursor: pointer;
+                .fleche {
+                    position: absolute;
+                    transition: 0.25s;
+                    opacity: 1;
+                    left: 215px;
+                }
+            }
+        }
+    }
+    .menu_link {
+        ul {
+            list-style-type: none;
+            padding: 0;
+            li {
+                font-size: 14px;
+                color: #777777;
+                line-height: 1.5;
+                margin: 6px 0;
+                transition: 0.25s;
+                @media screen and (max-width: 780px) {
+                    font-size: 16px;
+                    line-height: 1;
+                    padding: 12px 0;
+                }
+            }
+            li:hover {
+                color: #2B55A0;
+                transition: 0.25s;
+                cursor: pointer;
+            }
+        }
+        @media screen and (max-width: 780px) {
+            display: none;
+        }
+    }
+    .cross__menu {
+        position: absolute;
+        width: 22px;
+        display: none;
+        right: 0;
+        top: 0;
+        z-index: 1;
+        cursor: pointer;
+        @media screen and (max-width: 780px) {
+            display: block;
+            right: -50px;
+            top: 25px;
+        }
+    }
+    #cross {
+        @media screen and (max-width: 780px) {
+            display: block;
+            right: 25px;
+            top: 25px;
+        }
+    }
+    #log__out {
+        display: none;
         justify-content: left;
         align-items: center;
         font-size: 16px;
-        font-weight:100;
-        color:white;
-        padding:12px 0;
-        margin:8px 0;
+        font-weight: 100;
+        color: white;
+        padding: 12px 0;
+        margin: -8px 0;
         background-color: #2B55A0;
-        width:235px;
-        padding-left:35px;
+        border-left: none;
+        width: 235px;
+        padding-left: 35px;
         border-top-right-radius: 25px;
         border-bottom-right-radius: 25px;
-        transition:.25s;
-        .fleche {
-          opacity:0;
-          left:195px;
+        transition: 0.25s;
+        p {
+            font-size: 16px;
+            margin: 0;
         }
-      }
-      li a:hover {
-        background-color:#F8880F;
-        transition:.25s;
-        width:250px;
-        cursor:pointer;
-        .fleche {
-          position:absolute;
-          transition:.25s;
-          opacity:1;
-          left:215px;
+        img {
+            height: 1.2em;
+            max-width: 100%;
+            filter:invert(100%);
+            margin-left: 5px;
         }
-      }
-    }
-  }
-  .menu_link {
-    ul {
-      list-style-type:none;
-      padding:0;
-      li {
-        font-size:14px;
-        color:#777777;
-        line-height:1.5;
-        margin:6px 0;
-        transition:.25s;
         @media screen and (max-width: 780px) {
-          font-size:16px;
-          line-height:1;
-          padding:12px 0;
+            display: flex;
         }
-      }
-      li:hover {
-        color:#2B55A0;
-        transition:.25s;
-        cursor:pointer;
-      }
     }
     @media screen and (max-width: 780px) {
-      display:none;
+        position: absolute;
+        left: -300px;
+        transition: 1s;
+        z-index: 99;
     }
-  }
-  .cross__menu {
-    position:absolute;
-    width:22px;
-    display:none;
-    right:0;
-    top:0;
-    z-index:1;
-    cursor:pointer;
-    @media screen and (max-width: 780px) {
-      display:block;
-      right:-50px;
-      top:25px;
+    .social {
+        position: absolute;
+        bottom: 50px;
+        left: 50px;
+        img {
+            opacity: 0.25;
+            max-width: 25px;
+            margin-right: 10px;
+        }
+        img:hover {
+            opacity: 1;
+        }
     }
-  }
-  #cross {
-    @media screen and (max-width: 780px) {
-      display:block;
-      right:25px;
-      top:25px;
-    }
-  }
-  #log__out {
-    display:none;
-    justify-content: left;
-    align-items: center;
-    font-size: 16px;
-    font-weight:100;
-    color:white;
-    padding:12px 0;
-    margin:-8px 0;
-    background-color: #2B55A0;
-    border-left:none;
-    width:235px;
-    padding-left:35px;
-    border-top-right-radius: 25px;
-    border-bottom-right-radius: 25px;
-    transition:.25s;
-    p {
-      font-size:16px;
-      margin:0;
-    }
-    img {
-      height:1.2em;
-      max-width:100%;
-      filter:invert(100%);
-      margin-left:5px;
-    }
-    @media screen and (max-width: 780px) {
-      display:flex;
-    }
-  }
-  @media screen and (max-width: 780px) {
-    position:absolute;
-    left:-300px;
-    transition:1s;
-    z-index:99;
-  }
 }
-
-
 </style>
