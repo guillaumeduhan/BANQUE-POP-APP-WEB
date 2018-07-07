df871dbb1ec05e43763d3a89596f158b2b2001d7
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-07-07 23:54:25 +0200

    07/07/2018 à 23:54:19 MAJ NODE 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist/"));
 
 var port = process.env.PORT || 5000;
 
