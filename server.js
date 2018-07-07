88fa1e7db9b9d123b0353861d12f3a59e8574621
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-07-08 00:05:34 +0200

    08/07/2018 à 00:05:30 HEROKU

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var app = express();
 
-app = express();
 app.use(serveStatic(__dirname + "/dist/"));
+app.get(/.*/, function(req,res){
+  res.sendfile(__dirname + "/dist/index.html");
+});
 
 var port = process.env.PORT || 5000;
 
 app.listen(port);
 
-console.log('server started '+ port);
+console.log('server started ' + port);
